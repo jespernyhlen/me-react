acd39eac54f5d4505cc30654040ba20a668ce1f6
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-11 14:22:48 +0200

    Adding connection for string chat history

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import { insertMessage, getMessages } from './ChatAPI';
+
+const apiURL = 'https://me-api.jespernyhlenjs.me';
 
 class Chat extends Component {
     constructor(props) {
@@ -15,7 +18,6 @@ class Chat extends Component {
 
         this.socket = io('https://socket-server.jespernyhlenjs.me');
         // this.socket = io('http://localhost:8300');
-
         this.getTime = () => {
             let today = new Date();
             let months = [
@@ -45,20 +47,31 @@ class Chat extends Component {
 
         this.sendMessage = e => {
             e.preventDefault();
-            this.socket.emit('SEND_MESSAGE', {
+            let data = {
                 time: this.getTime(),
-                author: ' ' + this.state.username,
+                username: this.state.username,
                 message: this.state.message
-            });
+            };
+            this.socket.emit('SEND_MESSAGE', data);
+            insertMessage(data);
+
             this.setState({ message: '' });
         };
 
         this.joinChat = e => {
             e.preventDefault();
-            this.socket.emit('JOIN_CHAT', {
+            let data = {
                 time: this.getTime(),
-                message: this.state.username + ' has joined the chat'
+                username: this.state.username,
+                message: `${this.state.username} has joined the chat`
+            };
+            this.socket.emit('JOIN_CHAT', data);
+
+            insertMessage({
+                time: this.getTime(),
+                message: `${this.state.username} has joined the chat`
             });
+
             this.setState({
                 activeUser: true
             });
@@ -66,55 +79,57 @@ class Chat extends Component {
 
         this.leaveChat = e => {
             e.preventDefault();
-            this.socket.emit('LEAVE_CHAT', {
+            let data = {
                 time: this.getTime(),
-                message: this.state.username + ' has left the chat'
+                username: this.state.username,
+                message: `${this.state.username} has left the chat`
+            };
+            this.socket.emit('LEAVE_CHAT', data);
+            insertMessage({
+                time: this.getTime(),
+                message: `${this.state.username} has left the chat`
             });
+
             this.setState({
                 activeUser: false,
                 username: ''
             });
         };
 
-        this.socket.on('RECEIVE_JOIN_MESSAGE', function(data) {
-            joinMessage(data);
-        });
-
-        this.socket.on('RECEIVE_LEAVE_MESSAGE', function(data) {
-            leaveMessage(data);
-        });
-
-        this.socket.on('RECEIVE_MESSAGE', function(data) {
+        this.socket.on('NEW_MESSAGE', function(data) {
             addMessage(data);
+            // console.log(data);
         });
 
         const addMessage = data => {
             this.setState({ messages: [...this.state.messages, data] });
-            this.scrollToBottom();
-        };
 
-        const joinMessage = data => {
-            this.setState({
-                messages: [...this.state.messages, data]
-            });
-            this.scrollToBottom();
-        };
-
-        const leaveMessage = data => {
-            this.setState({
-                messages: [...this.state.messages, data]
-            });
             this.scrollToBottom();
         };
     }
 
     componentDidMount() {
+        fetch(apiURL + 'list')
+            .then(res => res.json())
+            .then(response => {
+                this.setState({
+                    messages: response
+                });
+            });
+    }
+
+    componentDidUpdate() {
+        // console.log(this.state.messages);
+
         this.scrollToBottom();
     }
 
     scrollToBottom = () => {
-        console.log(this.mesRef);
-        this.mesRef.current.scrollTop = this.mesRef.current.scrollHeight;
+        if (this.mesRef.current.scrollTop !== null) {
+            this.mesRef.current.scrollTop = this.mesRef.current.scrollHeight;
+        }
+
+        // console.log(this.mesRef);
     };
     render() {
         return (
@@ -132,8 +147,8 @@ class Chat extends Component {
                                     <p className='message-time'>
                                         {message.time}
                                     </p>
-                                    {message.author}
-                                    {message.author ? ':' : null}{' '}
+                                    {message.username}
+                                    {message.username ? ':' : null}{' '}
                                     <strong> {message.message}</strong>
                                 </div>
                             );
@@ -214,3 +229,197 @@ class Chat extends Component {
 }
 
 export default Chat;
+
+// import React, { Component, useEffect, useState, useRef } from 'react';
+// import io from 'socket.io-client';
+// import axios from 'axios';
+
+// import { insertMessage, getMessages } from './ChatAPI';
+// const socket = io('http://localhost:8300');
+
+// const Chat = () => {
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [activeUser, setActiveUser] = useState('');
+//     const [username, setUsername] = useState('');
+//     const [message, setMessage] = useState('');
+//     const [messages, setMessages] = useState([]);
+//     socket.on('connect', function() {
+//         console.info('Connected');
+//     });
+
+//     socket.on('disconnect', function() {
+//         socket.removeAllListeners();
+//     });
+//     const apiURL = 'http://localhost:8333/';
+
+//     const messagesEndRef = useRef(null);
+
+//     // this.socket = io('https://socket-server.jespernyhlenjs.me');
+//     const scrollToBottom = () => {
+//         messagesEndRef.current.scrollIntoView({ behavior: 'auto' });
+//     };
+//     const getTime = () => {
+//         let today = new Date();
+//         let months = [
+//             'Jan',
+//             'Feb',
+//             'Mar',
+//             'Apr',
+//             'May',
+//             'Jun',
+//             'Jul',
+//             'Aug',
+//             'Sep',
+//             'Oct',
+//             'Nov',
+//             'Dec'
+//         ];
+//         let month = months[today.getMonth()];
+//         let day = ('0' + today.getDate()).slice(-2);
+
+//         let time =
+//             ('0' + today.getHours()).slice(-2) +
+//             ':' +
+//             ('0' + today.getMinutes()).slice(-2);
+
+//         return month + ' ' + day + ' ' + time;
+//     };
+
+//     const sendMessage = e => {
+//         e.preventDefault();
+//         let data = {
+//             time: getTime(),
+//             username: username,
+//             message: message
+//         };
+//         socket.emit('SEND_MESSAGE', data);
+//         setMessage('');
+//         insertMessage(data);
+//     };
+
+//     const joinChat = e => {
+//         e.preventDefault();
+//         let data = {
+//             time: getTime(),
+//             username: username
+//         };
+
+//         socket.emit('JOIN_CHAT', data);
+//         setActiveUser(true);
+
+//         insertMessage(data);
+//     };
+
+//     const leaveChat = e => {
+//         e.preventDefault();
+//         let data = {
+//             time: getTime(),
+//             username: username
+//         };
+//         socket.emit('LEAVE_CHAT', data);
+//         setActiveUser(false);
+//         setUsername('');
+
+//         insertMessage(data);
+//     };
+
+//     socket.on('NEW_MESSAGE', function(data) {
+//         addMessage(data);
+//     });
+
+//     const addMessage = data => {
+//         setMessages([...messages, data]);
+//         scrollToBottom();
+//     };
+
+//     useEffect(scrollToBottom, [messages]);
+
+//     useEffect(() => {
+//         const fetchHistory = async () => {
+//             fetch(apiURL + 'list')
+//                 .then(res => res.json())
+//                 .then(response => {
+//                     setMessages(response);
+//                 });
+//         };
+//         fetchHistory();
+//     }, []);
+
+//     return (
+//         <main id='chat'>
+//             <div className='main-container register'>
+//                 <h1 className='center'>Chatt-klient</h1>
+//                 <p className='desc center'>
+//                     {' '}
+//                     Skriv in ett användarnamn för att delta i chatten
+//                 </p>
+//                 <div className='messages'>
+//                     {messages.map(message => {
+//                         return (
+//                             <div className='message-div'>
+//                                 <p className='message-time'>{message.time}</p>
+//                                 {message.username}
+//                                 {message.username ? ':' : null}{' '}
+//                                 <strong> {message.message}</strong>
+//                             </div>
+//                         );
+//                     })}
+//                     <div ref={messagesEndRef} />
+//                 </div>
+//                 <div className='form-group'>
+//                     {!activeUser ? (
+//                         <input
+//                             type='text'
+//                             placeholder='Username'
+//                             value={username}
+//                             onKeyDown={e => {
+//                                 if (e.keyCode == 13) {
+//                                     joinChat(e);
+//                                 }
+//                             }}
+//                             onChange={e => setUsername(e.target.value)}
+//                             className='form-control'
+//                         />
+//                     ) : null}
+//                     <br />
+//                     {activeUser ? (
+//                         <input
+//                             type='text'
+//                             placeholder='Message'
+//                             className='form-control'
+//                             value={message}
+//                             onKeyDown={e => {
+//                                 if (e.keyCode == 13) {
+//                                     sendMessage(e);
+//                                 }
+//                             }}
+//                             onChange={e => setMessage(e.target.value)}
+//                         />
+//                     ) : null}
+//                 </div>
+
+//                 <br />
+//                 <div className='form-group btns evenly'>
+//                     {activeUser ? (
+//                         <button onClick={sendMessage} className='btn register'>
+//                             Send
+//                         </button>
+//                     ) : null}
+//                     {activeUser ? (
+//                         <button onClick={leaveChat} className='btn reset'>
+//                             Leave
+//                         </button>
+//                     ) : null}
+
+//                     {!activeUser ? (
+//                         <button onClick={joinChat} className='btn register'>
+//                             Join
+//                         </button>
+//                     ) : null}
+//                 </div>
+//             </div>
+//         </main>
+//     );
+// };
+
+// export default Chat;
---
+++ src/components/Chat/ChatAPI.js
@@ -0,0 +1,63 @@
+// 'use strict';
+
+const apiURL =
+    process.env.NODE_ENV === 'local'
+        ? 'http://localhost:8333/'
+        : 'https://me-api.jespernyhlenjs.me/';
+
+// const apiURL = 'http://localhost:8333/';
+
+// const fs = require('fs');
+// const path = require('path');
+
+export function insertMessage(docs) {
+    console.log(docs);
+    // fetch('http://localhost:8333/reports', {
+    fetch(apiURL + 'insert', {
+        method: 'post',
+        body: JSON.stringify({
+            time: docs.time,
+            username: docs.username,
+            message: docs.message
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(function(res) {
+            console.log(res);
+            return res; //error here
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+export function getMessages() {
+    // fetch('http://localhost:8333/reports')
+    fetch(apiURL + 'list')
+        .then(res => res.json())
+        .then(response => returnMessages(response));
+
+    // console.log('hasdas');
+    // fetch(apiURL + 'list', {
+    //     method: 'get',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    //     .then(res => res.json())
+    //     .then(response => {
+    //         console.log('hej');
+    //     });
+}
+
+function returnMessages(response) {
+    let messages = [];
+    messages = response;
+
+    return messages;
+}
